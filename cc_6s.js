ba64fcc2e4a15b49178cfcccda1e5542f13d7149
linndonn <linndonn@usf.edu>
2025-02-09 19:45:50 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6s.js
+++ cc_6s.js
@@ -15,3 +15,15 @@ const calculateSalesTax = function(amount, taxRate) {
     /// (Test Data) - Testing the function
     calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7"
 calculateSalesTax(500, 0.1);  // Expected output: "Sales Tax: $50"
+
+// Task 3 - Employee Bonus Calculation
+//Write an arrow funciton calculateBonus(salary, performanceRating) that calculates bonus based on performance rating.
+const calculateBonus = (salary, performanceRating) => {
+    const bonusPercentage = {  "Excellent": 0.2, "Good": 0.1, "Average": 0.05
+    };//Calcualtes the 20%, 10% and 5% bonus based on the performance rating
+    //Logging the bonus amount
+const bonus = salary * (bonusPercentage[performanceRating]);
+console.log(`Bonus: $${bonus}`);};
+/// (Test Data) - Testing the function
+calculateBonus(5000, "Excellent"); // Expected output: "Bonus: $1000"
+calculateBonus(7000, "Good");      // Expected output: "Bonus: $700"
